a57534173500f333ef9e8492ff72a701857f8111
Beger Vilchez <begervilchez@gmail.com>
2016-02-04 03:10:22 +0000

    segundo commit

---
+++ indexbgr.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+	<title>Colegio de Psicólogos CDRII</title>
+	<link rel="stylesheet" href="style.css" type="text/css" charset="utf-8" />
+	<link rel="stylesheet" href="ie.css" type="text/css" charset="utf-8" />
+	<script language="Javascript" type="text/javascript" src="js/jquery-1.4.4.js"></script>
+	<script language="Javascript" type="text/javascript" src="js/jquery.lwtCountdown-1.0.js"></script>
+	<script language="Javascript" type="text/javascript" src="js/misc.js"></script>
+ 
+</head>
+<body>
+	<div id="wrapper">
+		<div id="logo">
+		</div> <!-- end of logo -->
+		<div id='titulo'>
+		  <br><br>
+		  <span>COLEGIO DE PSICÓLOGOS DEL PERÚ - CONSEJO DIRECTIVO REGIONAL II</span><br>
+		  <span>JUNÍN - AYACUCHO - HUANCAVELICA - APURIMAC</span><br>
+		  <span>Decano: Saúl Jesus Mallqui</span>
+		  <div id='aviso'>
+			  ESTAMOS LISTOS, EN UNOS MOMENTOS MOSTRAREMOS LA WEB!!
+<!-- 			  Estamos desarrollando esta página web para informarte mejor y lo podrás ver en: -->
+		  </div>
+		</div>
+		
+		<div id="main">
+			
+			<div id="countdown">
+				
+					<div class="dash weeks_dash">
+						<span class="dash_title">semanas</span>
+						<div class="digit">0</div>
+						<div class="digit">0</div>
+					</div>
+
+					<div class="dash days_dash">
+						<span class="dash_title">días</span>
+						<div class="digit">0</div>
+						<div class="digit">0</div>
+					</div>
+
+					<div class="dash hours_dash">
+						<span class="dash_title">horas</span>
+						<div class="digit">0</div>
+						<div class="digit">0</div>
+					</div>
+
+					<div class="dash minutes_dash">
+						<span class="dash_title">minutos</span>
+						<div class="digit">0</div>
+						<div class="digit">0</div>
+					</div>
+
+					<div class="dash seconds_dash">
+						<span class="dash_title">segundos</span>
+						<div class="digit">0</div>
+						<div class="digit">0</div>
+					</div>
+
+				
+					
+			</div> <!-- end of countdown -->
+			
+			<div id="bottom">
+				<div class="bottom-text">
+				Por ahora puedes contactarnos:  
+
+				</div>
+				<div class="bottom-text">
+	 
+				  <img src="images/mail.jpg" alt="Contactanos por correo">
+				</div>
+				
+				<div class='bottom-text2'>psicologos.cdr2@gmail.com</div>
+				<div class="bottom-text">
+				  <a href="https://www.facebook.com/psicologos.cdr2">
+				  <img src="images/facebook.jpg" alt="Facebook Page" />
+				  </a>
+				</div>
+				<div class='bottom-text2'>
+				  <a href="https://www.facebook.com/psicologos.cdr2">
+				  psicologos.cdr2
+				  </a>
+				</div>
+
+
+						<!--	<div id="social"> <!-- start of social icons list --> 
+				<!--	<ul>					
+					<li><a href="https://www.facebook.com/psicologos.cdr2"><img src="images/facebook.jpg" alt="Facebook Page" /></a></li>
+<!-- 					<li><a href="http://twitter.com/boogiesbc"><img src="images/twitter.jpg" alt="Twitter" /></a></li> -->
+<!-- 					<li><a href="http://ro.linkedin.com/pub/bogdan-sandu/18/a4a/8bb"><img src="images/linkedin.jpg" alt="LinkedIn Profile" /></a></li> -->
+					<!--<li><img src="images/mail.jpg" alt="Contactanos por correo" /><span class='bottom-text2'>psicologos.cdr2@gmail.com</span></li>
+					</ul>-->
+<!-- 				</div>--> <!-- end of social icons list -->  
+							
+<!-- 			</div> <!-- end of bottom div --> 
+
+			
+	
+		</div> <!-- end of main -->
+		<!-- start of the javascript code that handles the countdown -->
+		<script language="javascript" type="text/javascript">
+			jQuery(document).ready(function() {
+				$('#countdown').countDown({
+					targetDate: {
+						'day': 		24,
+						'month': 	4,
+						'year': 	2015,
+						'hour': 	18,
+						'min': 		20,
+						'sec': 		0
+					}
+				});
+							
+				
+				
+			});
+		</script>
+
+		
+			
+	</div> <!-- end of wrapper -->
+
+	<div class='center'>Desarrollador: <a href='https://www.facebook.com/begervilchez.psicologo'> Ps. Hans Beger Vilchez Carhuancho</a> (C.Ps.P. 17887)</div>
+
+
+</body>
+</html>
+
